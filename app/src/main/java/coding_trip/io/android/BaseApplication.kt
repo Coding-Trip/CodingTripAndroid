e8d8992776883a8e5345acf3b0db9eeb49355305
Ha10 <horiiortho5@gmail.com>
2018-02-10 11:25:59 +0900

    feat: Add Timber for log

--- app/src/main/java/coding_trip/io/android/BaseApplication.kt
+++ app/src/main/java/coding_trip/io/android/BaseApplication.kt
@@ -1,6 +1,18 @@
 package coding_trip.io.android
 
 import android.app.Application
+import timber.log.Timber
 
 class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+    }
+
+    private fun setupTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
